229618f9ce454c1cb4318805c82723a90139413f
Josh Kearns <jmdkearns@gmail.com>
2016-06-07 16:29:00 +0100

    Nine functions completed

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -13,3 +13,40 @@ end
 def multiply(first_number, second_number)
   return first_number * second_number
 end
+
+def divide(first_number, second_number)
+  return first_number / second_number
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string_1, string_2 )
+  return string_1 + string_2
+end
+
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
+
+def number_to_full_month_name(number)
+  case number
+  when 1
+    return "January"
+  when 3
+    return "March"
+  when 9 
+    return "September"
+  end
+end
+
+
+
+
+
+
+
+
+
+
